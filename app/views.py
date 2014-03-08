51027ff42063b2fbe2e9d56aaf172fea59755834
Tony Li <tonyli.lives@gmail.com>
2014-03-08 00:16:21 -0800

    basic page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {
+        'nickname': 'Miguel'
+    }
+    posts = [{
+        'author': {
+            'nickname': 'John'
+        },
+        'body': 'Beautify day in Vancouver (yeah right)!'
+    }, {
+        'author': {
+            'nickname': 'Susan'
+        },
+        'body': 'The Avengers movie was so cool!'
+    }]
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts
+                           )
