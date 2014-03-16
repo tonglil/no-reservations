0f11cc79f7fdd5dc7a88fec2fc530029d96d4a35
Tony Li <tonyli.lives@gmail.com>
2014-03-16 16:53:38 -0700

    boostrapped!

--- app/views.py
+++ app/views.py
@@ -1,41 +1,11 @@
-from flask import render_template, flash, redirect
+from flask import render_template
 from app import app
-from forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
+#@app.route('/login', methods=['GET', 'POST'])
 def index():
-    user = {
-        'nickname': 'Miguel'
-    }
-    posts = [{
-        'author': {
-            'nickname': 'John'
-        },
-        'body': 'Beautify day in Vancouver (yeah right)!'
-    }, {
-        'author': {
-            'nickname': 'Susan'
-        },
-        'body': 'The Avengers movie was so cool!'
-    }]
     return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts
-                           )
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenId="' + form.openid.data +
-              '", remember_me=' + str(form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS']
+                           title='Home'
                            )
