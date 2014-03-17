a5109090d4dd2dc05004651f5213acdfb4a3e517
Tony Li <tonyli.lives@gmail.com>
2014-03-16 19:35:48 -0700

    Search page layout done
    Added bootswatch theme
    Working on different pages
    Then will move onto connecting routes using ajax

--- app/views.py
+++ app/views.py
@@ -1,11 +1,59 @@
 from flask import render_template
 from app import app
 
+user = 'hi'
+#user = None
+
+
+@app.route('/style')
+def style():
+    return render_template('style.html',
+                           title='Styles',
+                           user=user
+                           )
+
 
 @app.route('/')
-@app.route('/index')
-#@app.route('/login', methods=['GET', 'POST'])
 def index():
     return render_template('index.html',
-                           title='Home'
+                           title='Home',
+                           user=user
                            )
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    return render_template('search.html',
+                           title='Search',
+                           user=user
+                           )
+
+
+@app.route('/search', methods=['POST'])
+def results():
+    return render_template('results.html',
+                           title='Search Results',
+                           user=user
+                           )
+#@app.route('/checkout')
+#@app.route('/returns')
+
+
+
+#@app.route('/item/new')
+#@app.route('/item/add')
+#@app.route('/item/:item/hold')
+#@app.route('/item/:item/checkout')
+#@app.route('/item/:item/return')
+
+
+
+#@app.route('/user/new')
+#@app.route('/user/add')
+#@app.route('/user/:user')
+#@app.route('/user/:user/fines')
+
+
+
+##overdue, checkedout, popular
+#@app.route('/report/:report')
