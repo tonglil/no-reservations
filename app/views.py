89634ccbecf241195587dc38d6a3141a53e2e417
Tony Li <tonyli.lives@gmail.com>
2014-03-17 00:43:38 -0700

    complete all templates except for reports

--- app/views.py
+++ app/views.py
@@ -1,16 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 user = 12
-#user = None
-
-
-@app.route('/style')
-def style():
-    return render_template('style.html',
-                           title='Styles',
-                           user=user
-                           )
 
 
 @app.route('/')
@@ -35,12 +26,31 @@ def results():
                            title='Search Results',
                            user=user
                            )
-#@app.route('/checkout')
-#@app.route('/returns')
+
+
+@app.route('/checkout')
+def checkout():
+    return render_template('admin/checkout.html',
+                           title='Checkout Items',
+                           user=user
+                           )
+
+
+@app.route('/returns')
+def returns():
+    return render_template('admin/returns.html',
+                           title='Process Returns',
+                           user=user
+                           )
 
 
 
-#@app.route('/item/new')
+@app.route('/item/new')
+def itemNew():
+    return render_template('admin/new.html',
+                           title='New Item',
+                           user=user
+                           )
 #@app.route('/item/add')
 #@app.route('/item/:item/hold')
 #@app.route('/item/:item/checkout')
@@ -49,14 +59,14 @@ def results():
 
 
 @app.route('/borrower/new')
-def borrowerNewAccount():
+def borrowerNew():
     return render_template('borrower/new.html',
                            title='New Borrower Account',
                            user=user
                            )
 #@app.route('/borrower/add')
 @app.route('/borrower/<int:borrower_id>')
-def borrowerGetAccount(borrower_id):
+def borrowerAccount(borrower_id):
     return render_template('borrower/account.html',
                            title='Account Information',
                            user=user
